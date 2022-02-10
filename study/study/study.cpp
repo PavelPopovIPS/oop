bb69618555d7bb7ec69096afea2f243dbfa041b4
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-02-10 21:25:41 +0300

    создал репозиторий

---
+++ study/study/study.cpp
@@ -0,0 +1,9 @@
+// study.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+}
