eae3cdc0516e92be8b49408912859b60e43a8c95
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-02-11 00:03:11 +0300

    Добавил код для копирования файлов

--- study/study/study.cpp
+++
@@ -1,9 +0,0 @@
-// study.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-void main(int argc, char* argv[])
-{
-    std::cout << "Hello World!\n";
-}
