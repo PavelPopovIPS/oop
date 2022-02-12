07d53625c7f2129f813b11482f11268860fd9221
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-02-12 21:25:22 +0300

    Добавил оставшиеся тесты

--- study/study/tests.bat
+++ study/study/tests.bat
@@ -13,7 +13,31 @@ REM Copy empty file
 %MyProgram% "empty.txt" "%TEMP%\output.txt" || goto err
 fc "empty.txt" "%TEMP%\output.txt" || goto err
 
+REM Copy file with one line
+%MyProgram% "one-line.txt" "%TEMP%\output.txt" || goto err
+fc "one-line.txt" "%TEMP%\output.txt" || goto err
+
+REM Copy file with one empty line
+%MyProgram% "textWithEmptyLine.txt" "%TEMP%\output.txt" || goto err
+fc "textWithEmptyLine.txt" "%TEMP%\output.txt" || goto err
+
+REM Copy missing file should fail
+%MyProgram% "missingFile.txt" "%TEMP%\output.txt" && goto err
+echo It is Ok!
+echo:
+
+REM Start program with one arg
+%MyProgram% "missingFile.txt" && goto err
+echo It is Ok!
+echo:
+
+REM Input Output files are not specified
+%MyProgram% && goto err
+echo It is Ok!\
+echo:
+
 REM Тесты прошли успешно
+echo Tests passed successfuly
 exit /B 0
 
 REM Выведем ошибку если тесты не прошли
