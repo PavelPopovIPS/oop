7aeb6c1de813132d5202b122118c8550f27219ec
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-02-12 18:11:13 +0300

    Выделил парсинг параметров командной строки в отдельную функцию. Применил использование функции в программе

--- study/study/copyFile.cpp
+++ study/study/copyFile.cpp
@@ -1,86 +1,84 @@
-// study.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// study.cpp : This file contains the 'main' function. Program execution begins
+// and ends there.
 //
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <optional>
+#include <string>
+
+struct Args
+{
+	std::string inputFileName;
+	std::string outputFileName;
+};
+
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+	if (argc != 3)
+	{
+		return std::nullopt;
+	}
+	Args args;
+	args.inputFileName = argv[1];
+	args.outputFileName = argv[2];
+	return args;
+}
 
 void CopyStream(std::istream& input, std::ostream& output)
 {
-
-}
-
-int Add(int a, int b)
-{
-
-}
-
-void Swap(int& a, int& b)
-{
-    int tmp = a;
-    a = b;
-    b = tmp;
-}
-
-void AppendEndOfLine(std::string& s)
-{
-    s += '\n';
 }
 
 int main(int argc, char* argv[])
 {
-    std::string s = "hello";
-    AppendEndOfLine(s);
-    int x = 42;
-    int y = 13;
-    Swap(x, y);
+	auto args = ParseArgs(argc, argv);
+	//Проверяем необходимое количество аргументов
+	if (!args)
+	{
+		std::cout << "Invalid argument count\n";
+		std::cout << "Usage: CopyFile.exe <input file name> <output file name>\n";
+		return 1;
+	}
 
-    //Проверяем необходимое количество аргументов
-    if (argc != 3) {
-        std::cout << "Error was occured\n";
-        std::cout << "Must be 2 arguments: study.exe <input file name> <output file name>\n";
-        //Поменять название программы.
-        return 1;
-    }
+	// Открываем файл для чтения
+	std::ifstream input;
+	input.open(args->inputFileName);
+	if (!input.is_open())
+	{
+		std::cout << "File " << args->inputFileName << " did not opened for reading\n";
+		return 1;
+	}
 
-    // Открываем файл для чтения
-    std::ifstream input;
-    input.open(argv[1]);
-    if (!input.is_open())
-    {
-        std::cout << "File " << argv[1] << " did not opened for reading\n";
-        return 1;
-    }
+	// Открываем файл для чтения
+	std::ofstream output(args->outputFileName);
+	// output.open();
+	if (!output.is_open())
+	{
+		std::cout << "File " << args->outputFileName << " did not opened for writining\n";
+		return 1;
+	}
 
-    // Открываем файл для чтения
-    std::ofstream output;
-    output.open(argv[2]);
-    if (!output.is_open())
-    {
-        std::cout << "File " << argv[2] << " did not opened for writining\n";
-        return 1;
-    }
+	// Копируем содержимое файла
+	char ch;
+	while (input.get(ch))
+	{
+		if (!output.put(ch))
+		{
+			break;
+		}
+	}
 
-    // Копируем содержимое файла 
-    char ch;
-    while (input.get(ch)) 
-    {
-        if (!output.put(ch)) 
-        {
-            break; 
-        }
-    }
+	if (input.bad())
+	{
+		std::cout << "Failed to read data from file\n";
+		return 1;
+	}
 
-    if (input.bad())
-    {
-        std::cout << "Failed to read data from file\n";
-        return 1;
-    }
+	if (!output.flush())
+	{
+		std::cout << "Failed to write to file\n";
+		return 1;
+	}
 
-    if (!output.flush())
-    {
-        std::cout << "Failed to write to file\n";
-        return 1;
-    }
-
-    return 0;
+	return 0;
 }
