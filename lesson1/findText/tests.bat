a27becc1466d586a492e2d3c19a4952865639482
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-02-15 21:18:48 +0300

    findText Добавил тесты поиска текста в файле

--- lesson1/findText/tests.bat
+++ lesson1/findText/tests.bat
@@ -9,13 +9,55 @@ if %MyProgram%=="" (
 	exit /B 1
 )
 
-REM App should not compare text if got one argument
-%MyProgram% "emptyFile1.txt"
-if NOT ERRORLEVEL 1 goto err
+REM Text should be found in the beginning of file 
+%MyProgram% "text-en.txt" "No" > NUL
+if ERRORLEVEL 1 goto err
+echo Test 1: passed successful
 
-REM App should not compare text without arguments
-%MyProgram%
+REM Text should be found in the end of file 
+%MyProgram% "text-en.txt" "less." > NUL
+if ERRORLEVEL 1 goto err
+echo Test 2: passed successful
+
+REM Text should be found in the middle of file 
+%MyProgram% "text-en.txt" "main" > NUL
+if ERRORLEVEL 1 goto err
+echo Test 3: passed successful
+
+REM The same text should be found in multiple lines of file 
+%MyProgram% "text-en.txt" "the" > NUL
+if ERRORLEVEL 1 goto err
+echo Test 4: passed successful
+
+REM Text with spaces should be found
+%MyProgram% "text-en.txt" "a piece of the continent" > NUL
+if ERRORLEVEL 1 goto err
+echo Test 5: passed successful
+
+REM Empty line should be found
+%MyProgram% "text-en.txt" "" > NUL
+if ERRORLEVEL 1 goto err
+echo Test 6: passed successful
+
+REM App should case sensitivity and text should not be found
+%MyProgram% "text-en.txt" "Main" > NUL
 if NOT ERRORLEVEL 1 goto err
+echo Test 7: passed successful
+
+REM Text should not be found in emtpty file
+%MyProgram% "emptyText.txt" "again" > NUL
+if NOT ERRORLEVEL 1 goto err
+echo Test 8: passed successful
+
+REM App should not search text if got one argument
+%MyProgram% "text1.txt" > NUL
+if NOT ERRORLEVEL 1 goto err
+echo Test 9: passed successful
+
+REM App should not search text without arguments
+%MyProgram% > NUL
+if NOT ERRORLEVEL 1 goto err
+echo Test 10: passed successful
 
 REM Тесты прошли успешно
 echo Tests passed successfuly
