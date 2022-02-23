046a8a7aa5940306caf853b003887cefd8b33a65
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-02-23 18:19:22 +0300

    [flipbyte] Поменял кодировку

--- lesson1/flipbyte/flipbyte.cpp
+++ lesson1/flipbyte/flipbyte.cpp
@@ -1,4 +1,4 @@
-// flipbyte.cpp : This file contains the 'main' function. Program execution begins and ends there.
+п»ї// flipbyte.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <bitset>
@@ -48,59 +48,59 @@ unsigned int FlipByte(const unsigned char& byte)
 
 	unsigned int newByte;
 
-	// Распечатал полученный байт // debug
+	// Р Р°СЃРїРµС‡Р°С‚Р°Р» РїРѕР»СѓС‡РµРЅРЅС‹Р№ Р±Р°Р№С‚ // debug
 	std::bitset<8> bitset1{ byte }; // debug
 	std::cout << bitset1 << std::endl; // debug
 
-	// Разделил бит на половинки и поменял их местами
+	// Р Р°Р·РґРµР»РёР» Р±РёС‚ РЅР° РїРѕР»РѕРІРёРЅРєРё Рё РїРѕРјРµРЅСЏР» РёС… РјРµСЃС‚Р°РјРё
 	// left is  43210000
 	// right is 00008765
 	unsigned char left = byte << 4;
 	unsigned char right = byte >> 4;
 
-	// Склеил половинки // debug
+	// РЎРєР»РµРёР» РїРѕР»РѕРІРёРЅРєРё // debug
 	newByte = left | right; // debug
-	// Распечатал склеенные половинки
+	// Р Р°СЃРїРµС‡Р°С‚Р°Р» СЃРєР»РµРµРЅРЅС‹Рµ РїРѕР»РѕРІРёРЅРєРё
 	std::bitset<8> bitset6{ newByte }; // debug
 	std::cout << "change 1/2 " << bitset6 << std::endl; // debug
 
-	//Разделил левую половину на четверти и поменял их местами
+	//Р Р°Р·РґРµР»РёР» Р»РµРІСѓСЋ РїРѕР»РѕРІРёРЅСѓ РЅР° С‡РµС‚РІРµСЂС‚Рё Рё РїРѕРјРµРЅСЏР» РёС… РјРµСЃС‚Р°РјРё
 	// left1 is 21000000
 	// left2 is 00430000
 	unsigned char left1 = left << 2;
 	unsigned char left2 = left >> 2;
-	left2 &= 48; // 48 - маска в двоичной системе 00110000
+	left2 &= 48; // 48 - РјР°СЃРєР° РІ РґРІРѕРёС‡РЅРѕР№ СЃРёСЃС‚РµРјРµ 00110000
 
-	// Поменял местами биты в четвертях
+	// РџРѕРјРµРЅСЏР» РјРµСЃС‚Р°РјРё Р±РёС‚С‹ РІ С‡РµС‚РІРµСЂС‚СЏС…
 	// left1 is 12000000
 	// left2 is 00340000
 	left1 = (left1 << 1) | ((left1 >> 1) & 64);
 	left2 = ((left2 << 1) & 32) | ((left2 >> 1) & 16);
 
-	// Склеил левые четвертинки
+	// РЎРєР»РµРёР» Р»РµРІС‹Рµ С‡РµС‚РІРµСЂС‚РёРЅРєРё
 	left = left1 | left2; // debug
 	std::bitset<8> bitset4{ left }; // debug
 	std::cout << bitset4 << std::endl; // debug
 
-	//Разделил правую половину на четверти и поменял их местами
+	//Р Р°Р·РґРµР»РёР» РїСЂР°РІСѓСЋ РїРѕР»РѕРІРёРЅСѓ РЅР° С‡РµС‚РІРµСЂС‚Рё Рё РїРѕРјРµРЅСЏР» РёС… РјРµСЃС‚Р°РјРё
 	// right1 is 00006500
 	// right2 is 00000087
 	unsigned char right1 = right << 2;
-	right1 &= 12; // 12 - маска в двоичной системе 00001100
+	right1 &= 12; // 12 - РјР°СЃРєР° РІ РґРІРѕРёС‡РЅРѕР№ СЃРёСЃС‚РµРјРµ 00001100
 	unsigned char right2 = right >> 2;
 
-	// Поменял местами биты в четвертях
+	// РџРѕРјРµРЅСЏР» РјРµСЃС‚Р°РјРё Р±РёС‚С‹ РІ С‡РµС‚РІРµСЂС‚СЏС…
 	// right1 is 00005600
 	// right2 is 00000078
 	right1 = (right1 << 1) & 8 | ((right1 >> 1) & 4);
 	right2 = ((right2 << 1) & 2) | (right2 >> 1);
 
-	// Склеил правые четвертинки
+	// РЎРєР»РµРёР» РїСЂР°РІС‹Рµ С‡РµС‚РІРµСЂС‚РёРЅРєРё
 	right = right1 | right2; // debug
 	std::bitset<8> bitset5{ right }; // debug
 	std::cout << bitset5 << std::endl; // debug
 
-	// Склеил половинки и распечатал
+	// РЎРєР»РµРёР» РїРѕР»РѕРІРёРЅРєРё Рё СЂР°СЃРїРµС‡Р°С‚Р°Р»
 	newByte = left | right; // debug
 	std::bitset<8> bitset7{ newByte }; // debug
 	std::cout << "change 1/4 " << bitset7 << std::endl; // debug
