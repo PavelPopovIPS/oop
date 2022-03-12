48fe2f2d5b9f4e6a055e9b6e51250333f28d6de5
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-03-12 21:12:30 +0300

    AddNumberToVectorElements_tests  Изменил кодировку

--- lab02/AddNumberToVectorElements_tests/AddNumberToVectorElements_tests.cpp
+++ lab02/AddNumberToVectorElements_tests/AddNumberToVectorElements_tests.cpp
@@ -1,6 +1,11 @@
-// AddNumberToVectorElements_tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+п»ї// AddNumberToVectorElements_tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#define CATCH_CONFIG_MAIN // убирает main и говорит catch запускать только этот файл. Должен быть выже include catch
+#define CATCH_CONFIG_MAIN // СѓР±РёСЂР°РµС‚ main Рё РіРѕРІРѕСЂРёС‚ catch Р·Р°РїСѓСЃРєР°С‚СЊ С‚РѕР»СЊРєРѕ СЌС‚РѕС‚ С„Р°Р№Р». Р”РѕР»Р¶РµРЅ Р±С‹С‚СЊ РІС‹Р¶Рµ include catch
 #include "../../catch2/catch.hpp"
 #include <iostream>
+
+TEST_CASE("test")
+{
+	REQUIRE(1 == 1);
+}
