3bda36406f69e2d0054ba5fb0a8bb694b11b6f82
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-03-12 19:06:35 +0300

    AddNumberToVectorElements_tests Добавил проект тестов для AddNumberToVectorElements

---
+++ lab02/AddNumberToVectorElements_tests/AddNumberToVectorElements_tests.cpp
@@ -0,0 +1,6 @@
+// AddNumberToVectorElements_tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define CATCH_CONFIG_MAIN // убирает main и говорит catch запускать только этот файл. Должен быть выже include catch
+#include "../../catch2/catch.hpp"
+#include <iostream>
