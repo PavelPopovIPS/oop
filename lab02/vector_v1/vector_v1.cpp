b04eee6a27592c9afeebfcc10c40b540c734c47d
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-03-12 15:54:46 +0300

    vector_v1 Написал программу Vector, которая читает из input введеную информацию , кладет ее в вектор и печатает всозданный вектор

--- lab02/vector_v1/vector_v1.cpp
+++ lab02/vector_v1/vector_v1.cpp
@@ -1,9 +1,63 @@
-// vector_v1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// vector_v1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+std::string ReadInput()
+{
+	std::string inputStr;
+	std::getline(std::cin, inputStr);
+
+	return inputStr;
+}
+
+std::vector<double> ParseStringToVector(const std::string& inputStr)
+{
+	std::vector<double> vectr;
+	std::istringstream strm(inputStr);
+	for (std::string elem; std::getline(strm, elem, ' ');)
+	{
+		try
+		{
+			vectr.push_back(std::stod(elem));
+		}
+		catch (std::invalid_argument& e)
+		{
+			throw std::runtime_error("You should use number\n");
+		}
+		catch (std::out_of_range& e)
+		{
+			throw std::runtime_error("Number is toooo big\n");
+		}
+	}
+
+	return vectr;
+}
+
+void PrintVector(const std::vector<double>& vectr)
+{
+	for (double elem : vectr)
+	{
+		std::cout << elem << std::endl;
+	}
+}
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	try
+	{
+		std::string inputStr = ReadInput();
+		std::vector<double> vectr = ParseStringToVector(inputStr);
+		PrintVector(vectr);
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
 }
