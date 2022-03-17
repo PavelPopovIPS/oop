97ded921bf2fd54b19bfebc98d9a3146a38caa82
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-03-17 23:26:22 +0300

    wordCounter Реализовал функции CountWordInLine и PrintMap

--- lab02/wordCount/wordCount.cpp
+++ lab02/wordCount/wordCount.cpp
@@ -2,6 +2,8 @@
 //
 
 #include <iostream>
+#include <map>
+#include <sstream>
 #include <string>
 
 std::string ReadInput()
@@ -11,7 +13,32 @@ std::string ReadInput()
 	return text;
 }
 
+std::map<std::string, int> CountWordInLine(std::string& text)
+{
+	std::map<std::string, int> wordCounter;
+	std::istringstream strm(text);
+	std::string word;
+
+	while (strm >> word)
+	{
+		++wordCounter[word];
+	}
+
+	return wordCounter;
+}
+
+void PrintMap(std::map<std::string, int>& wordCounter)
+{
+	for (const std::pair<std::string, int>& pair : wordCounter)
+	{
+		std::cout << pair.first << ": " << pair.second << std::endl;
+	}
+}
+
 int main()
 {
 	std::string text = ReadInput();
+	std::map<std::string, int> wordCounter = CountWordInLine(text);
+	PrintMap(wordCounter);
+	return 0;
 }
