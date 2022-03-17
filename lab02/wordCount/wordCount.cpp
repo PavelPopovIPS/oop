3bfa98076ef622bc2675800ec587eb0f7b943966
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-03-17 21:23:15 +0300

    worldCount Создал проект

---
+++ lab02/wordCount/wordCount.cpp
@@ -0,0 +1,17 @@
+// wordCount.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+std::string ReadInput()
+{
+	std::string text;
+	std::getline(std::cin, text);
+	return text;
+}
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
