5965fdee434f7f6339757c49c8e0dcdba29aad66
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-03-23 20:55:23 +0300

    dic_tests Добавил проект тестов для словарика

---
+++ lab02/dictionary_tests/dictionary_tests.cpp
@@ -0,0 +1,6 @@
+// dictionary_tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "../../catch2/catch.hpp"
+#include "../dictionary/dicfns.h"
