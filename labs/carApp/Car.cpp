3ca6e2704901da5658e894062a26aca28afe3246
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-03-27 17:57:53 +0300

    Car добавил заклушки в класс Car, добавил проект с тестами

--- labs/carApp/Car.cpp
+++ labs/carApp/Car.cpp
@@ -1,2 +1,43 @@
 #include "stdafx.h"
 #include "Car.h"
+
+bool Car::IsTurnedOn() const
+{
+	return false;
+}
+
+Direction Car::GetDirection() const
+{
+	Direction direction = Direction::Stop;
+	return direction;
+}
+
+int Car::GetSpeed() const
+{
+	return 0;
+}
+
+int Car::GetGear() const
+{
+	return 0;
+}
+
+bool Car::TurnOnEngine()
+{
+	return false;
+}
+
+bool Car::TurnOffEngine()
+{
+	return false;
+}
+
+bool Car::SetGear(int gear)
+{
+	return false;
+}
+
+bool Car::SetSpeed(int speed)
+{
+	return false;
+}
