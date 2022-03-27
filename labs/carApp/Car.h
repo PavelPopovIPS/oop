5dee4012deb546c26ef12cadf57120031b1d0ae1
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-03-27 17:43:46 +0300

    car описал класс Car в заголовочном файле

--- labs/carApp/Car.h
+++ labs/carApp/Car.h
@@ -1,1 +1,28 @@
 #pragma once
+
+enum class Direction
+{
+	Forward,
+	Back,
+	Stop,
+};
+
+class Car
+{
+public:
+	bool IsTurnedOn() const;
+	Direction GetDirection() const;
+	int GetSpeed() const;
+	int GetGear() const;
+
+	bool TurnOnEngine();
+	bool TurnOffEngine();
+	bool SetGear(int gear);
+	bool SetSpeed(int speed);
+
+private:
+	bool m_isEngineOn = false;
+	int m_gear = 0;
+	int m_speed = 0;
+	Direction m_direction = Direction::Stop;
+};
