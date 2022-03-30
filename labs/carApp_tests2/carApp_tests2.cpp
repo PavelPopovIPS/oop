c34c37e68e2077bfcd294bfce6e90e96bd884230
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-03-30 20:52:47 +0300

    car tests2 Написал сценарии для TurnOnEngine и TurnOffEngine

---
+++ labs/carApp_tests2/carApp_tests2.cpp
@@ -0,0 +1,216 @@
+﻿// carApp_tests2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "../../catch2/catch.hpp"
+#include "../carApp/Car.h"
+#include "../carApp/stdafx.h"
+
+SCENARIO("1. Проверка метода TurnOnEngine()")
+{
+	SECTION("1.1 Возвращает true, если двигатель включился")
+	{
+		Car car;
+
+		WHEN("двигатель включился")
+		{
+			bool result = car.TurnOnEngine();
+
+			THEN("метод возвращает true")
+			{
+				bool expectedResult = true;
+				REQUIRE(result == expectedResult);
+			}
+		}
+	}
+
+	SECTION("1.2 Возвращает true, если уже был включен.")
+	{
+		Car car;
+		car.TurnOnEngine();
+
+		WHEN("двигатель включается, во включенном состоянии")
+		{
+			bool result = car.TurnOnEngine();
+
+			THEN("метод возвращает true")
+			{
+				bool expectedResult = true;
+				REQUIRE(result == expectedResult);
+			}
+		}
+	}
+
+	SECTION("1.3 После включения двигателя, машина находится на нейтральной передаче, в состоянии покоя, скорость 0,")
+	{
+		Car car;
+		car.TurnOnEngine();
+
+		WHEN("двигатель включается")
+		{
+			bool state = car.IsTurnedOn();
+			Direction direction = car.GetDirection();
+			int gear = car.GetGear();
+			int speed = car.GetSpeed();
+
+			THEN("состояние двигателя - включено")
+			{
+				bool expectedResult = true;
+				REQUIRE(state == expectedResult);
+			}
+
+			THEN("машина находится в состоянии покоя")
+			{
+				Direction expectedResult = Direction::Stop;
+				REQUIRE(direction == expectedResult);
+			}
+
+			THEN("машина находится на нейтрали")
+			{
+				int expectedResult = 0;
+				REQUIRE(gear == expectedResult);
+			}
+
+			THEN("скорость 0")
+			{
+				int expectedResult = 0;
+				REQUIRE(speed == expectedResult);
+			}
+		}
+	}
+}
+
+SCENARIO("2. Проверка метода TurnOffEngine()")
+{
+
+	SECTION("2.1 Выключить двигатель, если он включен и текущая передача – нейтральная, а автомобиль стоит, Скорость  равна нулю - вернется true")
+	{
+		Car car;
+		car.TurnOnEngine();
+
+		WHEN("выключаю двигатель")
+		{
+			bool result = car.TurnOffEngine();
+			bool state = car.IsTurnedOn();
+
+			THEN("метод возвращает true")
+			{
+				bool expectedResult = true;
+				REQUIRE(result == expectedResult);
+			}
+
+			THEN("2.2 состояния двигателя выключено")
+			{
+				bool expectedResult = false;
+				REQUIRE(state == expectedResult);
+			}
+		}
+	}
+
+	SECTION("2.3 Выключить двигатель, если он уже выключен")
+	{
+		Car car;
+
+		WHEN("выключаю двигатель")
+		{
+			bool result = car.TurnOffEngine();
+			bool state = car.IsTurnedOn();
+
+			THEN("метод возвращает true")
+			{
+				bool expectedResult = true;
+				REQUIRE(result == expectedResult);
+			}
+
+			THEN("состояния двигателя выключено")
+			{
+				bool expectedResult = false;
+				REQUIRE(state == expectedResult);
+			}
+		}
+	}
+
+	SECTION("2.4 Выключить двигатель, если передача задняя")
+	{
+		Car car;
+		car.TurnOnEngine();
+		car.SetGear(-1);
+
+		WHEN("выключаю двигатель")
+		{
+			bool result = car.TurnOffEngine();
+			bool state = car.IsTurnedOn();
+
+			THEN("метод возвращает false")
+			{
+				bool expectedResult = false;
+				REQUIRE(result == expectedResult);
+			}
+
+			THEN("состояния двигателя включено")
+			{
+				bool expectedResult = true;
+				REQUIRE(state == expectedResult);
+			}
+		}
+	}
+
+	SECTION("2.5 Выключить двигатель, если передача первая")
+	{
+		Car car;
+		car.TurnOnEngine();
+		car.SetGear(1);
+
+		WHEN("выключаю двигатель")
+		{
+			bool result = car.TurnOffEngine();
+			bool state = car.IsTurnedOn();
+
+			THEN("метод возвращает false")
+			{
+				bool expectedResult = false;
+				REQUIRE(result == expectedResult);
+			}
+
+			THEN("состояния двигателя включено")
+			{
+				bool expectedResult = true;
+				REQUIRE(state == expectedResult);
+			}
+		}
+	}
+
+	SECTION("2.6 Выключить двигатель, если передача нейтральная и скорость ненулевая")
+	{
+		Car car;
+		car.TurnOnEngine();
+		car.SetGear(1);
+		car.SetSpeed(1);
+		car.SetGear(0);
+
+		WHEN("выключаю двигатель")
+		{
+			bool result = car.TurnOffEngine();
+			bool state = car.IsTurnedOn();
+			int speed = car.GetSpeed();
+
+			THEN("метод возвращает false")
+			{
+				bool expectedResult = false;
+				REQUIRE(result == expectedResult);
+			}
+
+			THEN("состояния двигателя включено")
+			{
+				bool expectedResult = true;
+				REQUIRE(state == expectedResult);
+			}
+
+			THEN("скорость равна заданному значению")
+			{
+				int expectedResult = 1;
+				REQUIRE(speed == expectedResult);
+			}
+		}
+	}
+}
