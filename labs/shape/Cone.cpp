2af8507f4ced9658d35dbffffd97bd33d5394dee
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-04-19 23:42:20 +0300

    Shape Написал клас cone

---
+++ labs/shape/Cone.cpp
@@ -0,0 +1,29 @@
+#include "Cone.h"
+
+CCone::CCone(double density, double baseRadius, double height)
+	: CBody("Cone", density)
+	, m_baseRadius(baseRadius)
+	, m_height(height)
+{
+}
+
+double CCone::GetBaseRadius() const
+{
+	return m_baseRadius;
+}
+
+double CCone::GetHeight() const
+{
+	return m_height;
+}
+
+double CCone::GetVolume() const
+{
+	return (1 / 3) * M_PI * pow(m_baseRadius, 2) * m_height;
+}
+
+void CCone::AppendProperties(std::ostream& strm) const
+{
+	strm << "\tbase radius = " << GetBaseRadius() << std::endl;
+	strm << "\theight = " << GetHeight() << std::endl;
+}
