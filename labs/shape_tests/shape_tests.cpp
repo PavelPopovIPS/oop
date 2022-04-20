56b2012b2521dfa061ba6be0fc13f8747bc25795
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-04-20 23:32:32 +0300

    shape tests написал первый тест для Sphere

--- labs/shape_tests/shape_tests.cpp
+++ labs/shape_tests/shape_tests.cpp
@@ -5,3 +5,23 @@
 #include "../../catch2/catch.hpp"
 #include "../shape/Sphere.h"
 #include "../shape/stdafx.h"
+
+SCENARIO("Tests for class Sphere")
+{
+	GIVEN("sphere with radius 3 and density 13")
+	{
+		double density = 3;
+		double radius = 13;
+		CSphere sphere(density, radius);
+		WHEN("get sphere radius")
+		{
+			double result = sphere.GetRadius();
+
+			THEN("radius should be equal 13")
+			{
+				double expectedResult = 13;
+				REQUIRE(result == expectedResult);
+			}
+		}
+	}
+}
