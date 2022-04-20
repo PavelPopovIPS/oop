61db4756d4b68da069b3b4def6011a0d7282ea85
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-04-20 23:18:04 +0300

    shape tests Добавил порект с тестами

---
+++ labs/shape_tests/shape_tests.cpp
@@ -0,0 +1,7 @@
+// shape_tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "../../catch2/catch.hpp"
+#include "../shape/Sphere.h"
+#include "../shape/stdafx.h"
