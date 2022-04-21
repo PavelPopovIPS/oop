a41e29d508d31b64d1df5aa0a4d6f0be89d5ccc5
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-04-21 22:42:57 +0300

    shape доработал тесты для классов фигур

--- labs/shape_tests/shape_tests.cpp
+++ labs/shape_tests/shape_tests.cpp
@@ -4,6 +4,8 @@
 #define CATCH_CONFIG_MAIN
 #include "../../catch2/catch.hpp"
 #include "../shape/Body.h"
+#include "../shape/Cone.h"
+#include "../shape/Cylinder.h"
 #include "../shape/Parallelepiped.h"
 #include "../shape/Sphere.h"
 #include "../shape/stdafx.h"
@@ -152,6 +154,144 @@ SCENARIO("Tests for class Parallelepiped")
 			THEN("mass should be equal ")
 			{
 				double expectedResult = 24.0;
+				REQUIRE(result == expectedResult);
+			}
+		}
+	}
+}
+
+SCENARIO("Tests for class Cone")
+{
+	GIVEN("cone with density is 3.3, base radius is 3 and height is 2.1")
+	{
+		double density = 3.3;
+		double baseRadius = 3;
+		double height = 2.1;
+
+		CCone cone(density, baseRadius, height);
+
+		WHEN("get density")
+		{
+			double result = cone.GetDensity();
+
+			THEN("density should be equal 3.3")
+			{
+				double expectedResult = 3.3;
+				REQUIRE(result == expectedResult);
+			}
+		}
+
+		WHEN("get baseRadius")
+		{
+			double result = cone.GetBaseRadius();
+
+			THEN("baseRadius should be equal 3")
+			{
+				double expectedResult = 3;
+				REQUIRE(result == expectedResult);
+			}
+		}
+
+		WHEN("get height")
+		{
+			double result = cone.GetHeight();
+
+			THEN("height should be equal 2.1")
+			{
+				double expectedResult = 2.1;
+				REQUIRE(result == expectedResult);
+			}
+		}
+
+		WHEN("get volume")
+		{
+			double result = cone.GetVolume();
+			result = round(result * 10) / 10;
+
+			THEN("volume should be equal 19.8")
+			{
+				double expectedResult = 19.8;
+				REQUIRE(result == expectedResult);
+			}
+		}
+
+		WHEN("get mass")
+		{
+			double result = cone.GetMass();
+			result = round(result * 10) / 10;
+
+			THEN("mass should be equal 65.3")
+			{
+				double expectedResult = 65.3;
+				REQUIRE(result == expectedResult);
+			}
+		}
+	}
+}
+
+SCENARIO("Tests for class Cylinder")
+{
+	GIVEN("cylinder with density is 1.2, base radius is 2.2 and height is 0.1")
+	{
+		double density = 1.2;
+		double baseRadius = 2.2;
+		double height = 0.1;
+
+		CCylinder cylinder(density, baseRadius, height);
+
+		WHEN("get density")
+		{
+			double result = cylinder.GetDensity();
+
+			THEN("density should be equal 1.2")
+			{
+				double expectedResult = 1.2;
+				REQUIRE(result == expectedResult);
+			}
+		}
+
+		WHEN("get baseRadius")
+		{
+			double result = cylinder.GetBaseRadius();
+
+			THEN("baseRadius should be equal 2.2")
+			{
+				double expectedResult = 2.2;
+				REQUIRE(result == expectedResult);
+			}
+		}
+
+		WHEN("get height")
+		{
+			double result = cylinder.GetHeight();
+
+			THEN("height should be equal 0.1")
+			{
+				double expectedResult = 0.1;
+				REQUIRE(result == expectedResult);
+			}
+		}
+
+		WHEN("get volume")
+		{
+			double result = cylinder.GetVolume();
+			result = round(result * 10) / 10;
+
+			THEN("volume should be equal 1.5")
+			{
+				double expectedResult = 1.5;
+				REQUIRE(result == expectedResult);
+			}
+		}
+
+		WHEN("get mass")
+		{
+			double result = cylinder.GetMass();
+			result = round(result * 10) / 10;
+
+			THEN("mass should be equal 1.8")
+			{
+				double expectedResult = 1.8;
 				REQUIRE(result == expectedResult);
 			}
 		}
