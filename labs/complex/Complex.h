771e45c8daa153bb2a738b7a6e5946a1696666d7
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-05-14 14:50:45 +0300

    Complex добавил конструктор

---
+++ labs/complex/Complex.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+class CComplex
+{
+public:
+	// инициализация комплексного числа значениями действительной и мнимой частей
+	CComplex(double real = 0, double image = 0);
+
+	// возвращает действительную часть комплексного числа
+	double Re() const;
+
+	// возвращает мнимую часть комплексного числа
+	double Im() const;
+
+	// возвращает модуль комплексного числа
+	double GetMagnitude() const;
+
+	// возвращает аргумент комплексного числа
+	double GetArgument() const;
+
+private:
+	double m_real;
+	double m_image;
+};
