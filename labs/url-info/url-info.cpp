40200fe6d102b0ff33153d1ec426606754c71783
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-05-30 22:24:38 +0300

    url вынес печать url в отдельную функцию PrintUrl

--- labs/url-info/url-info.cpp
+++ labs/url-info/url-info.cpp
@@ -4,6 +4,27 @@
 #include "HttpUrl.h"
 #include <iostream>
 #include <string>
+void PrintUrl(const CHttpUrl& httpUrl)
+{
+	std::cout << "URL is: " << httpUrl.GetURL() << std::endl;
+
+	std::cout << "Protocol is: ";
+
+	if (httpUrl.GetProtocol() == Protocol::HTTP)
+	{
+		std::cout << "http" << std::endl;
+	}
+
+	if (httpUrl.GetProtocol() == Protocol::HTTPS)
+	{
+		std::cout << "https" << std::endl;
+	}
+
+	std::cout << "Domain is: " << httpUrl.GetDomain() << std::endl;
+	std::cout << "Port is: " << httpUrl.GetPort() << std::endl;
+	std::cout << "Document is: " << httpUrl.GetDocument() << std::endl;
+	std::cout << std::endl;
+}
 
 int main()
 {
@@ -14,31 +35,11 @@ int main()
 		try
 		{
 			CHttpUrl httpUrl(url);
-
-			std::cout << "URL is: " << httpUrl.GetURL() << std::endl;
-
-			std::cout << "Protocol is: ";
-
-			if (httpUrl.GetProtocol() == Protocol::HTTP)
-			{
-				std::cout << "http" << std::endl;
-			}
-
-			if (httpUrl.GetProtocol() == Protocol::HTTPS)
-			{
-				std::cout << "https" << std::endl;
-			}
-
-			std::cout << "Domain is: " << httpUrl.GetDomain() << std::endl;
-			std::cout << "Port is: " << httpUrl.GetPort() << std::endl;
-			std::cout << "Document is: " << httpUrl.GetDocument() << std::endl;
 		}
 		catch (std::exception& e)
 		{
 			std::cout << e.what() << std::endl;
 		}
-
-		std::cout << std::endl;
 	}
 
 	return 0;
