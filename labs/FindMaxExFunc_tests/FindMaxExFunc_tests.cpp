a1b631cd26ec0a6245676960a9c1d40c4ee8850b
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-06-06 22:33:18 +0300

    FindMax tests написал тест поиска наибольшего атлета по росту

--- labs/FindMaxExFunc_tests/FindMaxExFunc_tests.cpp
+++ labs/FindMaxExFunc_tests/FindMaxExFunc_tests.cpp
@@ -5,3 +5,28 @@
 #include "../../catch2/catch.hpp"
 #include "../FindMaxExFunc/Athlete.h"
 #include "../FindMaxExFunc/FindMaxEx.h"
+
+bool operator==(const Athlete& a1, const Athlete& a2)
+{
+	bool isName = a1.name == a2.name;
+	bool isTall = a1.tall == a2.tall;
+	bool isWeight = a1.weight == a2.weight;
+	return (isName && isTall && isWeight);
+}
+
+TEST_CASE("Find max athlete by tall")
+{
+	std::vector<Athlete> athletes = {
+		{ "Ivan", 180, 80 },
+		{ "Elly", 161, 50 },
+		{ "Paul", 170, 100 },
+		{ "John", 195, 75 },
+	};
+
+	Athlete maxAthlete;
+
+	FindMax(athletes, maxAthlete, CompareTall);
+	Athlete expectedResult = { "John", 195, 75 };
+
+	REQUIRE(maxAthlete == expectedResult);
+}
