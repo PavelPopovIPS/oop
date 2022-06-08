e469a4e74ca2f08db59df3f3569eb700c7ee9e35
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-06-08 23:19:18 +0300

    mylist реализовал push_front и begin

---
+++ labs/MyListApp/CMyList.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+
+template <typename T>
+struct Node
+{
+	Node(const T& elem)
+		: m_elem(elem)
+	{
+	}
+
+	T m_elem;
+	std::shared_ptr<Node<T>> next = nullptr;
+	std::shared_ptr<Node<T>> prev = nullptr;
+};
+
+template <class T>
+class CMyList
+{
+public:
+	bool Push_front(const T& elem)
+	{
+		std::shared_ptr<Node<T>> node = std::make_shared<Node<T>>(elem);
+
+		if (m_pHead == nullptr && m_pTail == nullptr)
+		{
+			m_pHead = node;
+			m_pTail = node;
+		}
+		else
+		{
+			m_pHead->prev = node;
+			node->next = m_pHead;
+			m_pHead = node;
+		}
+
+		++m_count;
+		return true;
+	}
+
+	size_t Size() const
+	{
+		return m_count;
+	}
+
+	std::shared_ptr<Node<T>> Begin() const
+	{
+		return m_pHead;
+	}
+
+private:
+	std::shared_ptr<Node<T>> m_pHead = nullptr;
+	std::shared_ptr<Node<T>> m_pTail = nullptr;
+	size_t m_count = 0;
+};
+
+template <class T>
+std::ostream& operator<<(std::ostream& stream, CMyList<T> const& list)
+{
+	auto it = list.Begin();
+
+	if (it == nullptr)
+	{
+		return stream;
+	}
+
+	while (it)
+	{
+		stream << it->m_elem;
+		it = it->next;
+		stream << std::endl;
+	}
+
+	return stream;
+}
--- labs/MyListApp/MyListApp.cpp
+++ labs/MyListApp/MyListApp.cpp
@@ -1,9 +1,15 @@
 // MyListApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "CMyList.h"
 #include <iostream>
+#include <sstream>
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	CMyList<int> list;
+	list.Push_front(1);
+	list.Push_front(5);
+
+	std::cout << list;
 }
