451d2aa06dafc89a85b44bb97ac8c9297c151f03
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-06-15 23:56:26 +0300

    mylist добавил проект с тестами

---
+++ labs/MyListApp_tests/MyListApp_tests.cpp
@@ -0,0 +1,6 @@
+// MyListApp_tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "../../catch2/catch.hpp"
+#include "../MyListApp/CMyList.h"
