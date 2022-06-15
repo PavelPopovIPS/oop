aa5f156dfe14b3fccbc086658ec4c3ff2bc20925
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2022-06-16 00:06:07 +0300

    mylist добавил два теста по добвалению числа

--- labs/MyListApp_tests/MyListApp_tests.cpp
+++ labs/MyListApp_tests/MyListApp_tests.cpp
@@ -4,3 +4,26 @@
 #define CATCH_CONFIG_MAIN
 #include "../../catch2/catch.hpp"
 #include "../MyListApp/CMyList.h"
+
+SCENARIO("create list conteiner for diferent types")
+{
+	WHEN("create list container for int")
+	{
+		CMyList<int> list;
+
+		THEN("shoul have ability to add int to list")
+		{
+			list.push_back(2);
+			REQUIRE(TRUE);
+		}
+
+		THEN("added element should be 2")
+		{
+			list.push_back(2);
+			auto it = list.begin();
+			int result = *it;
+			int expectedResult = 2;
+			REQUIRE(result == expectedResult);
+		}
+	}
+}
